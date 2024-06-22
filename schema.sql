e34a82c856750d34340a103d3ecced01496f1985
BoredPositron <97865180+BoredPositron@users.noreply.github.com>
2024-06-22 20:16:34 +0530

    schema

--- schema.sql
+++ schema.sql
@@ -7,3 +7,17 @@ CREATE TABLE IF NOT EXISTS users (
     username VARCHAR(50) NOT NULL,
     password VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE iplog (
+    sno INT AUTO_INCREMENT PRIMARY KEY,
+    ip VARCHAR(20) NOT NULL,
+    tstamp VARCHAR(50) NOT NULL,
+    type VARCHAR(20)
+);
+
+CREATE TABLE log_in_sessions(
+    sno INTO AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    tstamp varchar(50) NOT NULL,
+    ip VARCHAR(20) NOT NULL
+);
